6bb6a329bba57aa10833cbb21b9d39b441296e19
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-05 10:58:51 +0300

    Добавление функции которая ожидает сообщение от пользователя

--- base_telegram.py
+++ base_telegram.py
@@ -1,6 +1,7 @@
+from telethon import TelegramClient, events
 
 
-async def read_last_message(client, chat_username):
+async def read_last_message(client, chat_username): # функция которая считывает последнее сообщение
     try:
         chat = await client.get_entity(chat_username)  
         messages = await client.get_messages(chat, limit=1)
@@ -13,14 +14,14 @@ async def read_last_message(client, chat_username):
     except Exception as e:
             print(f"Ошибка при чтении последнего сообщения: {e}")
 
-async def send_message_to_user(client, username, message):
+async def send_message_to_user(client, username, message): # функция которая отправляет сообщение пользователю
     try:
         await client.send_message(username, message)
         print("Сообщение отправлено успешно.")
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
 
-async def read_and_send_last_message(client, chat_username, receiver_username):
+async def read_and_send_last_message(client, chat_username, receiver_username): # функция которая считывает и отправляет сообщение пользователю
     try:
         last_message = await read_last_message(client, chat_username)
         if last_message:
@@ -29,3 +30,18 @@ async def read_and_send_last_message(client, chat_username, receiver_username):
             print("В беседе нет сообщений.")
     except Exception as e:
         print(f"Произошла ошибка {e}")
+
+
+async def wait_for_message_from_user(client, user_id): # функция которая ждет сообщение от пользователя
+    text_message = ""
+    try:
+        @client.on(events.NewMessage)
+        async def handle_new_message(event):
+            if event.from_id == user_id:
+                print(f"Получено сообщение от пользователя с ID {user_id}: {event.text}")
+                text_message = event.text
+        await client.run_until_disconnected()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        return text_message
