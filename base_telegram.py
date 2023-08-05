4e3cfb321516f34d47f14a0332186c3c0fb85d08
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-05 10:42:29 +0300

    Убрал с кода await client.start() и await client.dissconnect(). За это будет отвечать with

--- base_telegram.py
+++ base_telegram.py
@@ -2,7 +2,6 @@
 
 async def read_last_message(client, chat_username):
     try:
-        await client.start()
         chat = await client.get_entity(chat_username)  
         messages = await client.get_messages(chat, limit=1)
         if messages:
@@ -13,22 +12,16 @@ async def read_last_message(client, chat_username):
             return None
     except Exception as e:
             print(f"Ошибка при чтении последнего сообщения: {e}")
-    finally:
-        await client.disconnect()
 
 async def send_message_to_user(client, username, message):
     try:
-        await client.start()
         await client.send_message(username, message)
         print("Сообщение отправлено успешно.")
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
-    finally:
-        await client.disconnect()
 
 async def read_and_send_last_message(client, chat_username, receiver_username):
     try:
-        await client.start()
         last_message = await read_last_message(client, chat_username)
         if last_message:
             await send_message_to_user(client, receiver_username, last_message)
@@ -36,5 +29,3 @@ async def read_and_send_last_message(client, chat_username, receiver_username):
             print("В беседе нет сообщений.")
     except Exception as e:
         print(f"Произошла ошибка {e}")
-    finally:
-        await client.disconnect()
--- parser_telegram.py
+++ parser_telegram.py
@@ -4,16 +4,16 @@ import dialogs
 import base_telegram
 import param
 
+# Функция для печати списка чатов и диалогов
+#with client:
+#   client.loop.run_until_complete(dialogs.print_dialogs(client))
+
 client = TelegramClient(param.password.name_session, param.password.api_id, param.password.api_hash, \
                         system_version=param.password.system_version, \
                         device_model=param.password.device_model, app_version=param.password.app_version)
 
 chat_username = param.username.get_message_username
 receiver_username = param.username.receiver_username
-
-# Функция для печати списка чатов и диалогов
-with client:
-   client.loop.run_until_complete(dialogs.print_dialogs(client))
 
 with client:
     client.loop.run_until_complete(base_telegram.read_and_send_last_message(client, chat_username, receiver_username))
