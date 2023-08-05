3e8eb8bb7eb0d6ade2b5dfecbe6c5d673e33909a
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-05 11:00:04 +0300

    Рефакторинг кода

--- parser_telegram.py
+++ parser_telegram.py
@@ -4,10 +4,6 @@ import dialogs
 import base_telegram
 import param
 
-# Функция для печати списка чатов и диалогов
-#with client:
-#   client.loop.run_until_complete(dialogs.print_dialogs(client))
-
 client = TelegramClient(param.password.name_session, param.password.api_id, param.password.api_hash, \
                         system_version=param.password.system_version, \
                         device_model=param.password.device_model, app_version=param.password.app_version)
@@ -15,7 +11,15 @@ client = TelegramClient(param.password.name_session, param.password.api_id, para
 chat_username = param.username.get_message_username
 receiver_username = param.username.receiver_username
 
-with client:
-    client.loop.run_until_complete(base_telegram.read_and_send_last_message(client, chat_username, receiver_username))
+with client: # функция которая ждет сообщение от пользователя
+    text_message = client.loop.run_until_complete(base_telegram.wait_for_message_from_user(client, param.username.get_message_username))
+
+with client: # функция которая отправляет сообщение пользователю
+    client.loop.run_until_complete(base_telegram.send_message_to_user(client, param.username.receiver_username, text_message))
 
 
+#with client: # Вызов функции для печати списка чатов и диалогов
+#   client.loop.run_until_complete(dialogs.print_dialogs(client))
+
+#with client: # Вызов функции которая читает последнее собщение и отправляет другому пользователю
+#    client.loop.run_until_complete(base_telegram.read_and_send_last_message(client, chat_username, receiver_username))
