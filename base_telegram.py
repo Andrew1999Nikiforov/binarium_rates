abeb6aad0a31d6d3c7f8d4958e46eabf1a0a32c3
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-09 08:47:06 +0300

    Удалил комментарии и лишний код, который уже не нужен

--- base_telegram.py
+++ base_telegram.py
@@ -1,6 +1,5 @@
 from telethon import events
 import socket
-import re
 
 async def read_last_message(client, chat_username): # функция которая считывает последнее сообщение
     try:
@@ -25,10 +24,6 @@ async def read_and_send_last_message(client, chat_username, receiver_username): 
     except Exception as e:
         print(f"Произошла ошибка {e}")
 
-async def check_exit(event, client): # выход из программы
-    if("exit" in str(event)): 
-        await client.disconnect()
-
 async def send_message_to_user(client, username, message): # функция которая отправляет сообщение пользователю
     try:
         await client.send_message(username, message)
@@ -36,29 +31,15 @@ async def send_message_to_user(client, username, message): # функция ко
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
 
-async def parse_message(message): # Функция которая обрабатывает строку с канала
-    pattern = r'([A-Za-z]+)\s+(\d{2}:\d{2})\s+(вверх|вниз)'
-    match = re.match(pattern, message)
-    if match:
-        return match.group(1), match.group(2), match.group(3)
-    else:
-        return False
-
-def send_text_to_program_y(text): # Функция отвечающая за отправку данных второй программе
+async def send_text_to_program_y(text): # Функция отвечающая за отправку данных второй программе
     server_address = ('localhost', 14777)  # Укажите адрес и порт сервера программы Y
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.sendto(text.encode('utf-8'), server_address)
 
-async def handle_new_message(event, client): # проверка сообщения на корректность
-    #await check_exit(event.text, client) # Выход из программы (Потом убрать)
-    #await send_message_to_user(client, param.username.receiver_username, event.text) # Пересылаем это письмо другому человеку (Потом убрать)
-    #print(f"Получено сообщение от пользователя с ID {event.chat.title}: {event.text}")
-    send_text_to_program_y(event.text)
-
 async def wait_for_message_from_user(client, user_id): # функция которая ждет сообщение от пользователя
     try:
         await client.start()
-        client.add_event_handler(lambda event: handle_new_message(event, client), events.NewMessage(chats=user_id))
+        client.add_event_handler(lambda event: send_text_to_program_y(event.text), events.NewMessage(chats=user_id))
         await client.run_until_disconnected()
     except Exception as e:
         print(f"Произошла ошибка: {e}")
