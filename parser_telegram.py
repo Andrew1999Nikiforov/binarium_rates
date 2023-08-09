828512e195cf0f6b428e6a434b9421f4591dc44c
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-09 08:46:28 +0300

    Удалил комментарии и лишний код, который уже не нужен

--- parser_telegram.py
+++ parser_telegram.py
@@ -8,13 +8,6 @@ client = TelegramClient(param.password.name_session, param.password.api_id, para
                         device_model=param.password.device_model, app_version=param.password.app_version)
 
 chat_username = param.username.get_message_username
-receiver_username = param.username.receiver_username
 
 with client: # функция которая ждет сообщение от пользователя
     client.loop.run_until_complete(base_telegram.wait_for_message_from_user(client, param.username.get_message_username))
-
-#with client: # функция которая отправляет сообщение пользователю
-#    client.loop.run_until_complete(base_telegram.send_message_to_user(client, param.username.receiver_username, "222"))
-
-#with client: # Вызов функции которая читает последнее собщение и отправляет другому пользователю
-#    client.loop.run_until_complete(base_telegram.read_and_send_last_message(client, chat_username, receiver_username))
